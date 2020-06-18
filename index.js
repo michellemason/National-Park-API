d2898883f90c47863016ba50398bf77e65acfb8d
Michelle Mason <65793421+michellemason@users.noreply.github.com>
2020-06-17 23:26:05 -0700

    actual NPS files upload

--- index.js
+++ index.js
@@ -1,97 +1,103 @@
 'use strict';
 
-//This funtion renders the results and diplays them in the DOM
-function displayResults(responseJson) {
-    console.log('displayResults ran');
-    //When data is ready - hides progress indicator
-    $(responseJson).ready(function () {
-        $('.loading').addClass('hidden')
-    })
-    console.log(responseJson)
-    console.log(responseJson.length)
-    console.log(responseJson[0].owner.login)
-    //Finds and displays username
-    let user = responseJson[0].owner.login
-    let userinfo = `
-        <h4>User: <span class="user">${user}</span></h4>
-        <h4><span class="user">Repos: ${responseJson.length}</span></h4>
-        <ul class="results-list"></ul>
-    `
-    $('.js-results').append(userinfo)
-    //loop through results and make a list of repos, including link and description
-    for (let i = 0; i < responseJson.length; i++) {
-        $('.js-results').append(`
-        <div class="result-item"><li><h4>${responseJson[i].name}</h4>
-        <a href="${responseJson[i].html_url}">${responseJson[i].html_url}</a>
-        <p>${responseJson[i].description}</p>
-        </li></div>
-        `)
-    }
-    //removes hidden class to display results
-    $('.js-results').removeClass('hidden')
+const apiKey = 'HxcBfaI7az3nTKbfSq5VY4zRbiJyw77UDw9sTnuX';
+const searchURL = 'https://developer.nps.gov/api/v1/parks';
 
-}
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
 
-//Fetches data from API, converts and passes data to be displayed in DOM
-function getRepos(username) {
-    console.log('getRepos ran');
-    //takes username parameter and adds to url
-    const url = `https://api.github.com/users/${username}/repos`
+  function getParks(query, limit=10) {
+    const params = {
+      api_key: apiKey,
+      stateCode: query,
+      limit,
+    };
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+  
     console.log(url);
-    //Asynchronous request to GitHub API
+  
     fetch(url)
-    //if repsonse is good, returns results in JSON format
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-    //if reponse is not ok,then throw an error
-            throw new Error(response.statusText);
-        })
-    //if reponse is ok, then we pass the JSON results into displayResults to be rendered in DOM
-        .then(responseJson => displayResults(responseJson))
-    //if reponse is not ok, then the error we threw will be passed as a parameter in the displayError function and rendered in DOM
-        .catch(err => {
-            displayError(err.message);
-        });
-}
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => displayResults(responseJson))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
 
-//Takes thrown error as parameter and displays in DOM
-function displayError(error) {
-    console.log('displayError ran');
-    $('.js-results').html(`<h3 class="error">Something went wrong: ${error}</h3>`)
-    $('.loading').addClass('hidden');
-    $('.js-results').removeClass('hidden')
-}
-//This function uses random to select the text of the Search Button
-function getSearchPhrase() {
-    return (['Search', 'Find','Look up','Go','Check','Push this button','Don\'t push this button'])[Math.floor(Math.random() * 7)];
-}
-
-//Event listener for submit event
-function watchForm() {
-    //Listens for submit event
-    $('#js-form').submit(event => {
-        //override default behavior
-        event.preventDefault();
-        console.log('watchForm ran');
-        //determine the text of the button
-        let searchPhrase = getSearchPhrase()
-        console.log(searchPhrase)
-        //change the text of the search button
-        $('#find-btn').html(searchPhrase)
-        //clears any prior data from results section
-        $('.js-results').empty().addClass('hidden')
-        //store username 
-        const username = $('.js-username').val();
-        console.log(username);
-        //This utilizes setTimeout function to test progress indicator animation
-        $('.loading').removeClass('hidden');
-        setTimeout(function () {
-            //pass username to API call 
-            getRepos(username);
-        }, 1000)
+  function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchTerm = $('#js-search-term').val();
+      const maxResults = $('#js-max-results').val();
+      getParks(searchTerm, maxResults);
     });
-}
+  }
+  
+  $(watchForm);
+  
 
-$(watchForm);
+
+
+
+  function displayResults(responseJson) {
+    // if there are previous results, remove them
+    $('#results-list').empty();
+
+    if (responseJson.data.length == 0) {
+        alert('Sorry, no parks with that state abbreviation found.');
+    }
+    // iterate through the items array
+    for (let i = 0; i < responseJson.data.length; i++){
+      // for each park object in the items 
+      //array, add a list item to the results 
+      //list with the park full name, descriptionbrack,
+      //and website URL, address 
+
+      // get the physical address
+      const addressesObj = responseJson.data[i].addresses;
+      var physicalAddress = {};
+      for (let j = 0; j < addressesObj.length; j++) {
+        if (addressesObj[j].type == "Physical"){
+            physicalAddress = addressesObj[j];
+        }
+      }
+
+      //  put the parts of the address I want into a string array
+      var addressArray = [];
+      addressArray.push(physicalAddress.line1);
+      addressArray.push(physicalAddress.line2);
+      addressArray.push(physicalAddress.line3);
+      addressArray.push(physicalAddress.city);
+      addressArray.push(physicalAddress.stateCode);
+      addressArray.push(physicalAddress.postalCode);
+
+      // then format a string based on the array
+      var addressString = "";
+      for (let j = 0; j < addressArray.length; j++) {
+          if (addressArray[j] != "") {
+            addressString += addressArray[j];
+            if (j != addressArray.length - 1) {
+                addressString += ", ";
+            }
+          }
+      }
+
+      $('#results-list').append(
+        `<li><h3>${responseJson.data[i].fullName}</h3>
+        <p>${addressString}</p>
+        <p>${responseJson.data[i].description}</p>
+        <a href='${responseJson.data[i].url}'>${responseJson.data[i].url}</a>
+        </li>`
+      )};
+    //display the results section  
+    $('#results').removeClass('hidden');
+  }; 
